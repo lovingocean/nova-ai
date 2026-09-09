7c67b3369a94b5fbd6777e16b9dea505f70e3cdf
Iftikhar Ahmad <meekifti@gmail.com>
2026-09-09 17:26:48 +0500

    fix: dedupe react to fix useState null error

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
+    dedupe: ['react', 'react-dom'],
   },
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
-        warn(warning)
-      }
-    }
-  }
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
