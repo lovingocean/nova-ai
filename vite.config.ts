1b26a7bc36f4752e0e1fc09f9779b52ae2e37ab9
Iftikhar Ahmad <meekifti@gmail.com>
2026-09-16 11:08:53 +0500

    fix: simplify vite config, remove path causing errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      'react': resolve(__dirname, 'node_modules/react'),
-      'react-dom': resolve(__dirname, 'node_modules/react-dom'),
-    },
     dedupe: ['react', 'react-dom'],
   },
   optimizeDeps: {
     include: ['react', 'react-dom'],
-    force: true,
   },
 })
